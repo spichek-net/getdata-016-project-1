ebbdae53bfabfac49b22c21db39117066eb4fd24
Vlad Spichek <vspichek@gmail.com>
2014-12-21 12:09:23 -0800

    first commit

---
+++ run_analysis.R
@@ -0,0 +1,52 @@
+library(dplyr)
+
+sourceURL <- "http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+tmpFile <- tempfile(); download.file(sourceURL ,tmpFile)
+
+readFile <- function(fileName,...) {
+  if(tmpFile == FALSE )
+    read.delim(fileName,header=FALSE,sep="",...)
+  else
+    read.delim(unz(tmpFile,fileName),header=FALSE,sep="",...)
+}
+  
+readPart <- function(partName) {
+  baseDir <- "UCI HAR Dataset/"
+  headersFile  <- paste(sep="",baseDir,"features.txt")
+  labelsFile   <- paste(sep="",baseDir,"activity_labels.txt")
+  mainDataFile <- paste(sep="",baseDir,partName,"/X_",partName,".txt")
+  subjectsFile <- paste(sep="",baseDir,partName,"/subject_",partName,".txt")
+  activityFile <- paste(sep="",baseDir,partName,"/y_",partName,".txt")
+
+  activityLabels <- readFile(labelsFile,col.names = c("Activity ID","Activity"))
+
+  allHeaders <- readFile(headersFile,col.names = c("num","label"))
+  
+  allMetrics <- readFile(mainDataFile,col.names = allHeaders[,2])
+
+  metrics <- allMetrics[,grepl("mean|std",allHeaders$label)]
+  
+  subjects <- readFile(subjectsFile,col.names = c("Subject"))
+  
+  activityIDs <- readFile(activityFile,col.names = c("Activity ID"))
+  
+  Activity <- merge(activityIDs,activityLabels)[,2]
+  
+  cbind(subjects,Activity,metrics)
+}
+
+test <- readPart("test")
+train<- readPart("train")
+
+if(tmpFile != FALSE) unlink(tmpFile)
+
+all <- rbind(test,train)
+
+metricNames <- colnames(all)[3:81]
+meanExpressions <- paste(collapse=", ",gsub("(.*)","\"\\1-mean()\"=mean(`\\1`)",metricNames))
+
+averages <- eval(parse(text=paste(sep="","summarize(group_by(all,Subject,Activity),",meanExpressions,")")))
+
+write.table(averages,file="averages.txt", row.name=FALSE)
+
